83c718223d70155f7fcc2d2c83c3c646340fc0dc
Juri Lozowoj <juri.lozowoj@freenet.de>
2019-10-31 12:03:25 +0100

    Initial commit

---
+++ src/main/Rule.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Rule {
+
+    private Head head;
+    private Body body;
+
+    public Head addHead() {
+        this.head = new Head();
+        this.head.setRule(this);
+        return this.head;
+    }
+
+    public Body addBody(){
+        this.body = new Body();
+        return this.body;
+    }
+
+    public Head getHead(){
+        return this.head;
+    }
+
+    public Body getBody(){
+        return this.body;
+    }
+}
